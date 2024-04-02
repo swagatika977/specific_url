4d0eae933d6ddedfc36dabf96d116df525087ff6
swagatika977 <swagatikadasmohapatra02@gmail.com>
2024-04-02 08:56:40 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def communication(request):
+    return HttpResponse('For Job Communication is must needed')
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def english(request):
+    return render(request,'english.html')
